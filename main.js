ce51753e41e15df6cf31ceca655925adebf0d63c
songoku0000 <kfaizal000@gmail.com>
2024-10-24 15:42:51 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,70 @@
+const boxes = document.querySelectorAll(".cell");
+const winner = document.querySelector(".winner");
+const restart = document.querySelector(".Restart");
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+let turno = true;
+let count = 0;
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turno) {
+      box.innerText = "O";
+      turno = false;
+    } else {
+      box.innerText = "X";
+      turno = true;
+    }
+    box.disabled = true;
+    count++;
+
+    checkWinner();
+  });
+});
+
+restart.addEventListener("click", () => {
+  turno = true;
+  for (let box of boxes) {
+    winner.innerText = "";
+    box.innerText = "";
+    box.disabled = false;
+  }
+});
+
+const boxesDisabled = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+const checkWinner = () => {
+  for (pattern of winningConditions) {
+    let pos1 = boxes[pattern[0]].innerText;
+    let pos2 = boxes[pattern[1]].innerText;
+    let pos3 = boxes[pattern[2]].innerText;
+
+    if (pos1 != "" && pos2 != "" && pos3 != "") {
+      if (pos1 === pos2 && pos2 === pos3) {
+        boxesDisabled();
+        winner.innerText = `The Winner is ${pos1}`;
+      }
+    }
+  }
+};
+
+let isWinner = checkWinner();
+if (!isWinner && count === 9) {
+  draw();
+}
+const draw = () => {
+  winner.innerText = `DRAW`;
+  boxes.disabled = true;
+};
